c8fc6e75108aa51b5257fe8696c78d2241e1d72d
karinegassanova <20102374@mail.wit.ie>
2025-09-19 12:28:16 +0100

    Tasky Lab 2a - Exercise done

--- webapp2/src/App.jsx
+++ webapp2/src/App.jsx
@@ -1,14 +1,13 @@
-// App.js
 import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
 
 function App() {
-  const [ taskState, setTaskState ] = useState({
+  const [ taskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "High" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium" },
+      { id: 3, title: "Tidy up", deadline: "Today", priority: "Low" }
     ]
   });
 
@@ -16,13 +15,14 @@ function App() {
     <div className="container">
       <h1>Tasky</h1>
       {taskState.tasks.map((task) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-    />
-  ))} 
+        <Task 
+          key={task.id}
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          priority={task.priority}  
+        />
+      ))} 
     </div>
   );
 }
--- webapp2/src/components/Task.jsx
+++ webapp2/src/components/Task.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const Task = (props) => {
-    
-    return (
-        <div className="card">
-              <p className="title">{props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p>{props.description}</p>
-        </div>
-    )
+  return (
+    <div className="card">
+      <p className="title">{props.title}</p>
+      <p>Due: {props.deadline}</p>
+      <p>{props.description}</p>
+      <p>Priority: {props.priority}</p>
+    </div>
+  )
 }
-
-
 
 export default Task;
