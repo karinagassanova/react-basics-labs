d9d9b1e8c01567935e695f60aadd2d5e9874dc27
karinegassanova <20102374@mail.wit.ie>
2025-09-23 18:17:55 +0100

    Add task functionality

--- webapp2/src/components/Form.jsx
+++ webapp2/src/components/Form.jsx
@@ -1,28 +1,53 @@
 import React from "react";
 
 const AddTaskForm = (props) => {
-
   return (
     <div>
-      <form>
+      <form onSubmit={props.submit}>
         <label>
-            Task title:
-            <input type="text" name="title" required />
+          Task title:
+          <input
+            type="text"
+            name="title"
+            required
+            onChange={(event) => props.change(event)}/>
+        </label>
+        <br/>
+        <label>
+          Due date:
+          <input
+            type="date"
+            name="deadline"
+            required
+            onChange={(event) => props.change(event)}/>
         </label>
         <br />
         <label>
-            Due date:
-            <input type="date" name="deadline" required />
+          Details:
+          <input
+            type="text"
+            name="description"
+            onChange={(event) => props.change(event)}/>
         </label>
-        <br />
+        <br/>
         <label>
-            Details:
-            <input type="text" name="description" />
+          Priority: 
+          <br></br>
+          <select
+            name="priority"
+            required
+            onChange={(event) => props.change(event)}>
+            <option value="">Select priority</option>
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+          </select>
         </label>
+        <br/>
         <input type="submit" value="Submit" />
-        </form>
+      </form>
     </div>
-  )
+  );
 };
 
 export default AddTaskForm;
